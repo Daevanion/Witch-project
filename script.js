a8d286e0d02e99c4f64ea4da0d474f3d0b1a5560
Daevanion <166545774+Daevanion@users.noreply.github.com>
2025-05-28 20:11:28 +0300

    Added different JS file for logic

--- script.js
+++ script.js
@@ -1,51 +1,3 @@
-const delayMili = 4000;
-const delayMili_hide = 3500;
-const delayMili2 = 3000;
-const menuIcon = document.querySelector('#menu-icon');
-const navLinks = document.querySelector('.nav-links');
-
-menuIcon.onclick = () => {
-    navLinks.classList.toggle('active');
-}
-
-function conjureSpell(){
-    new Audio("button-click.mp3").play();
-    document.getElementById("theImage").src="witch_chant2.png";
-    document.getElementById("theImage").classList="img_active";
-    setTimeout(function(){
-        new Audio("click.mp3").play();
-    }, delayMili2);
-
-    setTimeout(function(){
-        document.getElementById("theImage").classList="hidden";
-    }, delayMili_hide);
-    
-    setTimeout(function(){
-        document.getElementById("theImage").src="idle_witch_animated.gif";
-        document.getElementById("theImage").classList="img_idle";
-    }, delayMili);
-
-};
-
-
-function banishSpell(){
-    new Audio("button-click.mp3").play();
-    document.getElementById("theImage").src="witch_banish.png";
-    document.getElementById("theImage").classList="img_active";
-    setTimeout(function(){
-    }, delayMili2);
-    
-    setTimeout(function(){
-        document.getElementById("theImage").classList="hidden";
-    }, delayMili_hide);
-    
-    setTimeout(function(){
-        document.getElementById("theImage").src="idle_witch_animated.gif";
-        document.getElementById("theImage").classList="img_idle";
-    }, delayMili);
-
-};
-
 
 class Spell  {
     constructor(spellnumber ,spellname, element, manaCost, category, skillImage){
@@ -149,6 +101,58 @@ function spellRandom(){
         console.log('not enough mana to proceed');
     }
 }
+
+
+
+
+const delayMili = 4000;
+const delayMili_hide = 3500;
+const delayMili2 = 3000;
+const menuIcon = document.querySelector('#menu-icon');
+const navLinks = document.querySelector('.nav-links');
+
+menuIcon.onclick = () => {
+    navLinks.classList.toggle('active');
+}
+
+function conjureSpell(){
+    new Audio("button-click.mp3").play();
+    document.getElementById("theImage").src="witch_chant2.png";
+    document.getElementById("theImage").classList="img_active";
+    setTimeout(function(){
+        new Audio("click.mp3").play();
+    }, delayMili2);
+
+    setTimeout(function(){
+        document.getElementById("theImage").classList="hidden";
+    }, delayMili_hide);
+    
+    setTimeout(function(){
+        document.getElementById("theImage").src="idle_witch_animated.gif";
+        document.getElementById("theImage").classList="img_idle";
+    }, delayMili);
+
+};
+
+
+function banishSpell(){
+    new Audio("button-click.mp3").play();
+    document.getElementById("theImage").src="witch_banish.png";
+    document.getElementById("theImage").classList="img_active";
+    setTimeout(function(){
+    }, delayMili2);
+    
+    setTimeout(function(){
+        document.getElementById("theImage").classList="hidden";
+    }, delayMili_hide);
+    
+    setTimeout(function(){
+        document.getElementById("theImage").src="idle_witch_animated.gif";
+        document.getElementById("theImage").classList="img_idle";
+    }, delayMili);
+
+};
+
 
 
 
