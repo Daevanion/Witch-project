17810500547dd1d43b1ad461fa70e733a5e39a11
Daevanion <166545774+Daevanion@users.noreply.github.com>
2025-05-28 20:07:42 +0300

    added logic behind spell crafting

--- script.js
+++ script.js
@@ -8,15 +8,6 @@ menuIcon.onclick = () => {
     navLinks.classList.toggle('active');
 }
 
-menuIcon.addEventListener('click',menuSway);
-
-
-function menuSway(){
-    if(navLinks == true)
-        menuIcon.classList.toggle('#menu-icon-moved');
-}
-
-
 function conjureSpell(){
     new Audio("button-click.mp3").play();
     document.getElementById("theImage").src="witch_chant2.png";
@@ -57,14 +48,111 @@ function banishSpell(){
 
 
 class Spell  {
-    constructor(affinity, ){
-
+    constructor(spellnumber ,spellname, element, manaCost, category, skillImage){
+        this.spellnumber = spellnumber;
+        this.spellname = spellname;
+        this.element = element;
+        this.manaCost = manaCost;
+        this.category = category;
+        this.skillImage = skillImage;
     }
 
 }
+
+
+
+
+function capFirst(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function getRandomInt(min, max) {
+  	return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function generateName(){
+	let name1 = ["blightrot",
+    "fireball",
+    "mindscorch",
+    "frostlung",
+    "wyrmpox",
+    "ashvein",
+    "gravebind",
+    "hexbane",
+    "doomrash",
+    "bloodwither",
+    "starcough",
+    "moondrain",
+    "voidcrack",
+    "ironblight",
+    "cursepox",
+    "darkfever",
+    "spiritflux",
+    "armour",
+    "magma spawn",
+    "kuma"];
+
+	let name2 = ["divinity", "archkana",
+    "isildore",
+    "death",
+    "the lost",
+    "aether",
+    "myrrak",
+    "valthur",
+    "xenorith",
+    "quireth",
+    "oshvara",
+    "drakzul",
+    "fenlath",
+    "flames",
+    "healing",
+    "scorching",
+    "korveth",
+    "iskareth",
+    "lumirae",
+    "thalorin",
+    "despair"];
+
+	var name = capFirst(name1[getRandomInt(0, name1.length)]) + ' of ' + capFirst(name2[getRandomInt(0, name2.length)]);
+	return name;
+
+}
+const mana = 50;
+let spellList = [];
+
+
+let spellindex = [];
+let spellno = 0
+
+
+
+
 
 function spellRandom(){
     if(mana > 0){
+        let spellno = (spellindex.length + 1);
 
+        
+        
+        function createObject(){
+            const name =`object${spellno}`;
+            spellindex[name] = new Spell(spellno, generateName());
+            
+            spellindex.push(name);
+            
+        }
+        createObject();
+
+    
+        
+    } else {
+        console.log('not enough mana to proceed');
     }
 }
+
+
+
+
+
+
+
